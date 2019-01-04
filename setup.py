77b7126fedbe18e12d2e74321e1446dcf6811041
Konstantin Baierer <unixprog@gmail.com>
2019-01-04 10:46:56 +0100

    basic python project setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='ocrd_anybaseocr',
+    version='0.0.1',
+    description='Tweaked ocropus scripts',
+    author='Saqib Bukhari',
+    author_email='saqib.bukhari@dfki.de',
+    url='https://github.com/kba/ocrd_dfkitools',
+    license='Apache License 2.0',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'ocrd-fork-ocropy>=1.4.0a1',
+    ],
+    packages=['ocrd_anybaseocr'],
+    entry_points={
+        'console_scripts': [
+            'ocrd-anybaseocr-binarize = ocrd_anybaseocr.cli.binarize',
+            'ocrd-anybaseocr-crop     = ocrd_anybaseocr.cli.cropping',
+            'ocrd-anybaseocr-deskew   = ocrd_anybaseocr.cli.deskew',
+        ]
+    },
+)
