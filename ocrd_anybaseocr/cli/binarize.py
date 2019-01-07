c5c39feb943a327c217e3d6ad6c765a4e78f2624
Konstantin Baierer <unixprog@gmail.com>
2019-01-07 16:42:06 +0100

    refactor binarize

--- ocrd_anybaseocr/cli/binarize.py
+++ ocrd_anybaseocr/cli/binarize.py
@@ -38,201 +38,148 @@
 #!/usr/bin/env python
 
 
-from pylab import *
-from numpy.ctypeslib import ndpointer
 import argparse
+import json
+import ocrolib
 import os
 import os.path
-import glob
-from scipy.ndimage import filters, interpolation, morphology, measurements
+import sys
+
+from pylab import amin, amax, mean, ginput, ones, clip, imshow, median, ion, gray, title, prod, minimum, array, clf
+from scipy.ndimage import filters, interpolation, morphology
 from scipy import stats
-import multiprocessing
-import ocrolib
-import json
-from pprint import pprint
-from xml.dom import minidom
-#import xml.etree.ElementTree
-#from lxml import etree
-
 
-def parseXML(fpath):
-    input_files = []
-    xmldoc = minidom.parse(fpath)
-    nodes = xmldoc.getElementsByTagName('mets:fileGrp')
-    for attr in nodes:
-        if attr.attributes['USE'].value == args.Input:
-            childNodes = attr.getElementsByTagName('mets:FLocat')
-            for f in childNodes:
-                input_files.append(f.attributes['xlink:href'].value)
-    return input_files
+from ..utils import parseXML, write_to_xml, print_info, parse_params_with_defaults, print_error
+from ..constants import OCRD_TOOL
 
+class OcrdAnybaseocrBinarizer():
 
-def write_to_xml(fpath):
-    xmldoc = minidom.parse(args.mets)
-    subRoot = xmldoc.createElement('mets:fileGrp')
-    subRoot.setAttribute('USE', args.Output)
+    def __init__(self, param):
+        self.param = param
 
-    for f in fpath:
-        #basefile = os.path.splitext(os.path.splitext(os.path.basename(f))[0])[0]
-        basefile = ocrolib.allsplitext(os.path.basename(f))[0]
-        child = xmldoc.createElement('mets:file')
-        child.setAttribute('ID', 'BIN_'+basefile)
-        child.setAttribute('GROUPID', 'P_' + basefile)
-        child.setAttribute('MIMETYPE', "image/png")
-
-        subChild = xmldoc.createElement('mets:FLocat')
-        subChild.setAttribute('LOCTYPE', "URL")
-        subChild.setAttribute('xlink:href', f)
-
-        # xmldoc.getElementsByTagName('mets:file')[0].appendChild(subChild);
-        subRoot.appendChild(child)
-        child.appendChild(subChild)
-
-    # subRoot.appendChild(child)
-    xmldoc.getElementsByTagName('mets:fileSec')[0].appendChild(subRoot)
-
-    if not args.OutputMets:
-        metsFileSave = open(os.path.join(
-            args.work, os.path.basename(args.mets)), "w")
-    else:
-        metsFileSave = open(os.path.join(args.work, args.OutputMets if args.OutputMets.endswith(
-            ".xml") else args.OutputMets+'.xml'), "w")
-    metsFileSave.write(xmldoc.toxml())
+    def check_page(self, image):
+        if len(image.shape) == 3:
+            return "input image is color image %s" % (image.shape,)
+        if mean(image) < median(image):
+            return "image may be inverted"
+        h, w = image.shape
+        if h < 600:
+            return "image not tall enough for a page image %s" % (image.shape,)
+        if h > 10000:
+            return "image too tall for a page image %s" % (image.shape,)
+        if w < 600:
+            return "image too narrow for a page image %s" % (image.shape,)
+        if w > 10000:
+            return "line too wide for a page image %s" % (image.shape,)
+        return None
 
 
-def print_info(*objs):
-    print("INFO: ", *objs, file=sys.stdout)
-
-
-def print_error(*objs):
-    print("ERROR: ", *objs, file=sys.stderr)
-
-
-def check_page(image):
-    if len(image.shape) == 3:
-        return "input image is color image %s" % (image.shape,)
-    if mean(image) < median(image):
-        return "image may be inverted"
-    h, w = image.shape
-    if h < 600:
-        return "image not tall enough for a page image %s" % (image.shape,)
-    if h > 10000:
-        return "image too tall for a page image %s" % (image.shape,)
-    if w < 600:
-        return "image too narrow for a page image %s" % (image.shape,)
-    if w > 10000:
-        return "line too wide for a page image %s" % (image.shape,)
-    return None
-
-
-def dshow(image, info):
-    if args.debug <= 0:
-        return
-    ion()
-    gray()
-    imshow(image)
-    title(info)
-    ginput(1, args.debug)
-
-
-def process1(job):
-    fname, i = job
-    print_info("# %s" % (fname))
-    if args.parallel < 2:
-        print_info("=== %s %-3d" % (fname, i))
-    raw = ocrolib.read_image_gray(fname)
-    dshow(raw, "input")
-    # perform image normalization
-    image = raw-amin(raw)
-    if amax(image) == amin(image):
-        print_info("# image is empty: %s" % (fname))
-        return
-    image /= amax(image)
-
-    if not args.nocheck:
-        check = check_page(amax(image)-image)
-        if check is not None:
-            print_error(fname+" SKIPPED. "+check +
-                        " (use -n to disable this check)")
+    def dshow(self, image, info):
+        if self.param.debug <= 0:
             return
-
-    # check whether the image is already effectively binarized
-    if args.gray:
-        extreme = 0
-    else:
-        extreme = (sum(image < 0.05)+sum(image > 0.95))*1.0/prod(image.shape)
-    if extreme > 0.95:
-        comment = "no-normalization"
-        flat = image
-    else:
-        comment = ""
-        # if not, we need to flatten it by estimating the local whitelevel
-        if args.parallel < 2:
-            print_info("flattening")
-        m = interpolation.zoom(image, args.zoom)
-        m = filters.percentile_filter(m, args.perc, size=(args.range, 2))
-        m = filters.percentile_filter(m, args.perc, size=(2, args.range))
-        m = interpolation.zoom(m, 1.0/args.zoom)
-        if args.debug > 0:
-            clf()
-            imshow(m, vmin=0, vmax=1)
-            ginput(1, args.debug)
-        w, h = minimum(array(image.shape), array(m.shape))
-        flat = clip(image[:w, :h]-m[:w, :h]+1, 0, 1)
-        if args.debug > 0:
-            clf()
-            imshow(flat, vmin=0, vmax=1)
-            ginput(1, args.debug)
-
-    # estimate low and high thresholds
-    if args.parallel < 2:
-        print_info("estimating thresholds")
-    d0, d1 = flat.shape
-    o0, o1 = int(args.bignore*d0), int(args.bignore*d1)
-    est = flat[o0:d0-o0, o1:d1-o1]
-    if args.escale > 0:
-        # by default, we use only regions that contain
-        # significant variance; this makes the percentile
-        # based low and high estimates more reliable
-        e = args.escale
-        v = est-filters.gaussian_filter(est, e*20.0)
-        v = filters.gaussian_filter(v**2, e*20.0)**0.5
-        v = (v > 0.3*amax(v))
-        v = morphology.binary_dilation(v, structure=ones((int(e*50), 1)))
-        v = morphology.binary_dilation(v, structure=ones((1, int(e*50))))
-        if args.debug > 0:
-            imshow(v)
-            ginput(1, args.debug)
-        est = est[v]
-    lo = stats.scoreatpercentile(est.ravel(), args.lo)
-    hi = stats.scoreatpercentile(est.ravel(), args.hi)
-    # rescale the image to get the gray scale image
-    if args.parallel < 2:
-        print_info("rescaling")
-    flat -= lo
-    flat /= (hi-lo)
-    flat = clip(flat, 0, 1)
-    if args.debug > 0:
-        imshow(flat, vmin=0, vmax=1)
-        ginput(1, args.debug)
-    bin = 1*(flat > args.threshold)
-
-    # output the normalized grayscale and the thresholded images
-    #print_info("%s lo-hi (%.2f %.2f) angle %4.1f %s" % (fname, lo, hi, angle, comment))
-    print_info("%s lo-hi (%.2f %.2f) %s" % (fname, lo, hi, comment))
-    if args.parallel < 2:
-        print_info("writing")
-    if args.debug > 0 or args.show:
-        clf()
+        ion()
         gray()
-        imshow(bin)
-        ginput(1, max(0.1, args.debug))
-    base, _ = ocrolib.allsplitext(fname)
-    ocrolib.write_image_binary(base+".bin.png", bin)
-    ocrolib.write_image_gray(base+".nrm.png", flat)
-    # print("########### File path : ", base+".nrm.png")
-    # write_to_xml(base+".bin.png")
-    return base+".bin.png"
+        imshow(image)
+        title(info)
+        ginput(1, self.param.debug)
+
+    def run(self, fname, i):
+        print_info("# %s" % (fname))
+        if self.param.parallel < 2:
+            print_info("=== %s %-3d" % (fname, i))
+        raw = ocrolib.read_image_gray(fname)
+        self.dshow(raw, "input")
+        # perform image normalization
+        image = raw-amin(raw)
+        if amax(image) == amin(image):
+            print_info("# image is empty: %s" % (fname))
+            return
+        image /= amax(image)
+
+        if not self.param.nocheck:
+            check = self.check_page(amax(image)-image)
+            if check is not None:
+                print_error(fname+" SKIPPED. "+check +
+                            " (use -n to disable this check)")
+                return
+
+        # check whether the image is already effectively binarized
+        if self.param.gray:
+            extreme = 0
+        else:
+            extreme = (sum(image < 0.05)+sum(image > 0.95))*1.0/prod(image.shape)
+        if extreme > 0.95:
+            comment = "no-normalization"
+            flat = image
+        else:
+            comment = ""
+            # if not, we need to flatten it by estimating the local whitelevel
+            if self.param.parallel < 2:
+                print_info("flattening")
+            m = interpolation.zoom(image, self.param.zoom)
+            m = filters.percentile_filter(m, self.param.perc, size=(self.param.range, 2))
+            m = filters.percentile_filter(m, self.param.perc, size=(2, self.param.range))
+            m = interpolation.zoom(m, 1.0/self.param.zoom)
+            if self.param.debug > 0:
+                clf()
+                imshow(m, vmin=0, vmax=1)
+                ginput(1, self.param.debug)
+            w, h = minimum(array(image.shape), array(m.shape))
+            flat = clip(image[:w, :h]-m[:w, :h]+1, 0, 1)
+            if self.param.debug > 0:
+                clf()
+                imshow(flat, vmin=0, vmax=1)
+                ginput(1, self.param.debug)
+
+        # estimate low and high thresholds
+        if self.param.parallel < 2:
+            print_info("estimating thresholds")
+        d0, d1 = flat.shape
+        o0, o1 = int(self.param.bignore*d0), int(self.param.bignore*d1)
+        est = flat[o0:d0-o0, o1:d1-o1]
+        if self.param.escale > 0:
+            # by default, we use only regions that contain
+            # significant variance; this makes the percentile
+            # based low and high estimates more reliable
+            e = self.param.escale
+            v = est-filters.gaussian_filter(est, e*20.0)
+            v = filters.gaussian_filter(v**2, e*20.0)**0.5
+            v = (v > 0.3*amax(v))
+            v = morphology.binary_dilation(v, structure=ones((int(e*50), 1)))
+            v = morphology.binary_dilation(v, structure=ones((1, int(e*50))))
+            if self.param.debug > 0:
+                imshow(v)
+                ginput(1, self.param.debug)
+            est = est[v]
+        lo = stats.scoreatpercentile(est.ravel(), self.param.lo)
+        hi = stats.scoreatpercentile(est.ravel(), self.param.hi)
+        # rescale the image to get the gray scale image
+        if self.param.parallel < 2:
+            print_info("rescaling")
+        flat -= lo
+        flat /= (hi-lo)
+        flat = clip(flat, 0, 1)
+        if self.param.debug > 0:
+            imshow(flat, vmin=0, vmax=1)
+            ginput(1, self.param.debug)
+        bin = 1*(flat > self.param.threshold)
+
+        # output the normalized grayscale and the thresholded images
+        #print_info("%s lo-hi (%.2f %.2f) angle %4.1f %s" % (fname, lo, hi, angle, comment))
+        print_info("%s lo-hi (%.2f %.2f) %s" % (fname, lo, hi, comment))
+        if self.param.parallel < 2:
+            print_info("writing")
+        if self.param.debug > 0 or self.param.show:
+            clf()
+            gray()
+            imshow(bin)
+            ginput(1, max(0.1, self.param.debug))
+        base, _ = ocrolib.allsplitext(fname)
+        ocrolib.write_image_binary(base+".bin.png", bin)
+        ocrolib.write_image_gray(base+".nrm.png", flat)
+        # print("########### File path : ", base+".nrm.png")
+        # write_to_xml(base+".bin.png")
+        return base+".bin.png"
 
 
 def main():
@@ -245,75 +192,24 @@ def main():
     and historical book pages.
     """)
 
-    parser.add_argument('-p', '--parameter', type=str,
-                        help="Parameter file location")
-    parser.add_argument('-w', '--work', type=str,
-                        help="Working directory location", default=".")
+    parser.add_argument('-p', '--parameter', type=str, help="Parameter file location")
+    parser.add_argument('-w', '--work', type=str, help="Working directory location", default=".")
     parser.add_argument('-I', '--Input', default=None, help="Input directory")
-    parser.add_argument('-n', '--nocheck', action="store_true",
-                        help="disable error checking on inputs")
-    parser.add_argument('-t', '--threshold', type=float,
-                        help='threshold, determines lightness, default: %(default)s')
-    parser.add_argument('-z', '--zoom', type=float,
-                        help='zoom for page background estimation, smaller=faster, default: %(default)s')
-    parser.add_argument('-e', '--escale', type=float,
-                        help='scale for estimating a mask over the text region, default: %(default)s')
-    parser.add_argument('-b', '--bignore', type=float,
-                        help='ignore this much of the border for threshold estimation, default: %(default)s')
-    parser.add_argument('-pe', '--perc', type=float,
-                        help='percentage for filters, default: %(default)s')
-    parser.add_argument('-r', '--range', type=int,
-                        help='range for filters, default: %(default)s')
-    parser.add_argument('-gr', '--gray', action='store_true',
-                        help='force grayscale processing even if image seems binary')
-    parser.add_argument(
-        '--lo', type=float, help='percentile for black estimation, default: %(default)s')
-    parser.add_argument(
-        '--hi', type=float, help='percentile for white estimation, default: %(default)s')
-    parser.add_argument('--debug', type=float,
-                        help='display intermediate results, default: %(default)s')
-    parser.add_argument('--show', action='store_true',
-                        help='display final result')
-    parser.add_argument('--rawcopy', action='store_true',
-                        help='also copy the raw image')
-    parser.add_argument('-O', '--Output', default=None,
-                        help="output directory")
+    parser.add_argument('-O', '--Output', default=None, help="output directory")
     parser.add_argument('-m', '--mets', default=None, help="METs input file")
-    parser.add_argument('-o', '--OutputMets', default=None,
-                        help="METs output file")
-    parser.add_argument('-g', '--group', default=None,
-                        help="METs image group id")
-    # parser.add_argument('files',nargs='+')
-    parser.add_argument('-Q', '--parallel', type=int)
+    parser.add_argument('-o', '--OutputMets', default=None, help="METs output file")
+    parser.add_argument('-g', '--group', default=None, help="METs image group id")
     args = parser.parse_args()
 
     # Read parameter values from json file
+    param = {}
     if args.parameter:
-        if not os.path.exists(args.parameter):
-            print("Error : Parameter file does not exists.")
-            sys.exit(0)
-        else:
-            param = json.load(open(args.parameter))
-    else:
-        if not os.path.exists('ocrd-anyBaseOCR-parameter.json'):
-            print("Error : Parameter file does not exists.")
-            sys.exit(0)
-        else:
-            param = json.load(open('ocrd-anyBaseOCR-parameter.json'))
-
-    args.threshold = param["anyBaseOCR"]["bin"]["threshold"]
-    args.zoom = param["anyBaseOCR"]["bin"]["zoom"]
-    args.escale = param["anyBaseOCR"]["bin"]["escale"]
-    args.bignore = param["anyBaseOCR"]["bin"]["bignore"]
-    args.perc = param["anyBaseOCR"]["bin"]["perc"]
-    args.range = param["anyBaseOCR"]["bin"]["range"]
-    args.lo = param["anyBaseOCR"]["bin"]["lo"]
-    args.hi = param["anyBaseOCR"]["bin"]["hi"]
-    args.debug = param["anyBaseOCR"]["bin"]["debug"]
-    args.parallel = param["anyBaseOCR"]["bin"]["parallel"]
+        with open(args.parameter, 'r') as param_file:
+            param = json.loads(param_file.read())
+    param = parse_params_with_defaults(param, OCRD_TOOL['tools']['ocrd-anybaseocr-deskew']['parameters'])
     # End to read parameters
 
-    # mendatory parameter check
+    # mandatory parameter check
     if not args.mets or not args.Input or not args.Output or not args.work:
         parser.print_help()
         print("Example: python ocrd-anyBaseOCR-binarize.py -m (mets input file path) -I (input-file-grp name) -O (output-file-grp name) -w (Working directory)")
@@ -326,8 +222,9 @@ def main():
         if not os.path.exists(args.work):
             os.mkdir(args.work)
 
-    files = parseXML(args.mets)
-    fname = []
-    for i, f in enumerate(files):
-        fname.append(process1((str(f), i+1)))
-    write_to_xml(fname)
+    binarizer = OcrdAnybaseocrBinarizer(param)
+    files = parseXML(args.mets, args.Input)
+    fnames = []
+    for i, fname in enumerate(files):
+        fnames.append(binarizer.run(str(fname), i+1))
+    write_to_xml(fnames, args.mets, args.Output, args.OutputMets, args.work)
